5a158144b875a41001adadf0c4732d7ae754658a
Fahim Karim <fahimk1@umbc.edu>
2022-03-11 21:03:15 -0500

    trying

--- simple_shell/simple_shell.c
+++ simple_shell/simple_shell.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
+#include "utils.h"
 
 
-#define MAX_LINE		80 
-
+#define MAX_LINE 80 
+#define EXITCD "exit"
 int main(void)
 {
 	char *args[MAX_LINE/2 + 1];
     	int should_run = 1;
 		
     	while (should_run){   
-        	printf("osh>");
-        	fflush(stdout);
-        
+        	printf("What do you want:");
+        	command = getline()
+			if(strcmp(EXITCD, command) ==0){
+				return 0;
+			}
         	/**
          	 * After reading user input, the steps are:
          	 * (1) fork a child process
