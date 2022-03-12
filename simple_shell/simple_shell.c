7a3256b7e35ff0d76ac166c1334da8641e68270a
Fahim Karim <fahimk1@umbc.edu>
2022-03-12 18:25:48 -0500

    a

--- simple_shell/simple_shell.c
+++ simple_shell/simple_shell.c
@@ -2,35 +2,66 @@
 #include <string.h>
 #include "utils.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define BUFFER_LEN 1024
 #define EXITCD "exit\n"
 
 
 int main(int argc, char *argv[]){
+	//variable stuff
 	char line[BUFFER_LEN];
+	char space[] = " ";
+	pid_t retVal;
 	int doYouRun = 1;
+	int status;
 
 	//first check if it was ran with arguments, if it was print error
 	if(argc > 1 && *argv != NULL){
 		fprintf( stderr, "it does not accept any command line arguments\n");
 		return 0;
 	}
-	//loop till exit
+
+	//loop till "exit" typed
 	while(doYouRun){
 		//prompt
 		printf("thing>");
 		fflush(stdout);
 		//read
-		if(!fgets(line, BUFFER_LEN, stdin)){  
+		if(!fgets(line, BUFFER_LEN, stdin)){ 
         	break;                                
     	}
+
 		//exit check
 		if (strcmp(line, EXITCD) == 0){
 			doYouRun = 0;
-		}
-		//if not then run thing
+		}else{
+			//if not then run thing
+			// goal is break into arg list, fork, then execvp, then wait
 		
+			//to do this first you have to break up the command by space
+			
+			// char *pnt = strtok(line, space);
+			// while(pnt != NULL){
+			// 	printf("%s \n", pnt);
+			// 	pnt = strtok(NULL, space);
+			// }
+
+			//now you can fork
+			retVal = fork();
+			//check if you're in child
+			if(retVal == 0){
+				execvp(line, line);
+				fprintf( stderr, "fail, Line56\n");
+				exit(0);
+			}else{
+				//else wait
+				while (wait(&status) != retVal);
+			}
+		}
+		
+
 	}
 	
 	return 0;
