4856de66a547e5cb26d2dcc81176e60f0770b3d1
Fahim Karim <fahimk1@umbc.edu>
2022-03-13 21:31:46 -0400

    part 1

--- simple_shell/simple_shell.c
+++ simple_shell/simple_shell.c
@@ -9,19 +9,38 @@
 #define EXITCD "exit\n"
 
 
+void  parse(char *line, char **argv)
+{
+     while (*line != '\0') {       /* if not the end of line ....... */ 
+          while (*line == ' ' || *line == '\t' || *line == '\n')
+               *line++ = '\0';     /* replace white spaces with 0    */
+          *argv++ = line;          /* save the argument position     */
+          while (*line != '\0' && *line != ' ' && 
+                 *line != '\t' && *line != '\n') 
+               line++;             /* skip the argument until ...    */
+     }
+     *argv = '\0';                 /* mark the end of argument list  */
+}
+
 int main(int argc, char *argv[]){
 	//variable stuff
 	char line[BUFFER_LEN];
-	char space[] = " ";
 	pid_t retVal;
 	int doYouRun = 1;
 	int status;
+	//char file_path[50];
+	//char* path= "/bin/";
+	//int spaceCount = 0;
+	char* argument[BUFFER_LEN];
+
 
 	//first check if it was ran with arguments, if it was print error
 	if(argc > 1 && *argv != NULL){
 		fprintf( stderr, "it does not accept any command line arguments\n");
 		return 0;
 	}
+
+	
 
 	//loop till "exit" typed
 	while(doYouRun){
@@ -40,48 +59,37 @@ int main(int argc, char *argv[]){
 			//if not then run thing
 			// goal is break into arg list, fork, then execvp, then wait
 		
-			//to do this first you have to break up the command by space
+			//1. remove /n
+			line[strcspn(line, "\n")] = 0;
 			
-			// char *pnt = strtok(line, space);
-			// while(pnt != NULL){
-			// 	printf("%s \n", pnt);
-			// 	pnt = strtok(NULL, space);
-			// }
+			//2. split into init comm
+			argument[0] = strtok(line, " ");
+			
+			//3. split rest of spaces
+			int i = 0;
+			while(argument[i] != NULL){
+				argument[++i] = strtok(NULL, " ");
+			}
+			
+
 
 			//now you can fork
 			retVal = fork();
 			//check if you're in child
 			if(retVal == 0){
-				execvp(line, line);
-				fprintf( stderr, "fail, Line56\n");
-				exit(0);
+				//execute
+				execvp(argument[0], argument);
+				fprintf( stderr, "fail, in child\n");
+				doYouRun = 0;
 			}else{
 				//else wait
 				while (wait(&status) != retVal);
 			}
 		}
 		
-
+		//spaceCount = 0;
 	}
 	
 	return 0;
 }
 
-// int main(void)
-// {
-// 	char *string;
-// 	char *args[MAX_LINE/2 + 1];
-// 	size_t size = 10;
-//     	int should_run = 1;
-		
-//     	while (should_run){   
-//         	printf("What do you want:");
-//         	char **string_pointer = &string;
-// 			size_t command = getline(string_pointer,&size,stdin);
-// 			if (strcmp(EXITCD, command) ==0){
-// 				return 0;
-// 			}
-//     	}
-    
-// 	return 0;
-// }
